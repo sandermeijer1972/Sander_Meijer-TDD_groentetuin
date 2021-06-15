5a642ca684db1c9badb2527a3ba95830bf38b2ab
Sander Meijer <sandertje1972@hotmail.com>
2021-06-15 11:03:01 +0200

    added second test

--- farm.js
+++ farm.js
@@ -1,10 +1,19 @@
-const getCostsForCrop = (input) => {
-    const costsForOnePlant = 1;
+const costsForOnePlant = 1;
+
+const getCostsForCrop = (input) => {    
     const totalCosts = input.numCrops * costsForOnePlant;
     console.log(totalCosts);
     return totalCosts;
 };
 
+const getRevenueForCrop = (input) => {
+    const revenueOfOnePlant = input.crop.salePrice * input.crop.yield;
+    const revenueForCrop = revenueOfOnePlant * input.numCrops;
+    console.log(revenueForCrop);
+    return revenueForCrop;
+};
+
 module.exports = {
     getCostsForCrop,
+    getRevenueForCrop,
 };
--- farm.test.js
+++ farm.test.js
@@ -1,5 +1,6 @@
 const {
     getCostsForCrop,
+    getRevenueForCrop,
 } = require("./farm.js");
 
 describe("testing getCostsForCrop", () => {
@@ -8,5 +9,20 @@ describe("testing getCostsForCrop", () => {
     };
     test("get costs for crop", () => {
         expect(getCostsForCrop(input)).toBe(80);
+    });
+});
+
+describe("testing getRevenueForCrop", () => {
+    const strawberry = {
+        name: "strawberry",
+        salePrice: 4,
+        yield: 3,
+    };
+    const input = {
+        crop: strawberry,
+        numCrops: 20,
+    };
+    test("get Revenue for crop", () => {
+        expect(getRevenueForCrop(input)).toBe(240);
     });
 });
