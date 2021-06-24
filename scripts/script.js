59806ee4eb1bd1999171594b6887e6c75eed0eea
Sander Meijer <sandertje1972@hotmail.com>
2021-06-24 12:11:25 +0200

    added HTML CSS and scripts

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const vulButton = document.getElementById("vulbutton");
+const lijn = document.getElementById("lijn");
+const vakkenArea = document.getElementById("vakken");
+const factorArea = document.getElementById("factors");
+const rekenButton = document.getElementById("rekenbutton");
+const resultArea = document.getElementById("result");
+
